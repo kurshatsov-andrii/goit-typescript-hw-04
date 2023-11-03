c8dfdea2f9cf35f8adf420cf607aa5480197c4c0
Andrii Kurshatsov <andreswebit@ukr.net>
2023-11-03 18:47:59 +0200

    Add task 1

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef } from 'react';
+type Props = {
+  onContentEndVisible: () => void;
+  children: ReactNode;
+};
 
-// Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
+    const options: IntersectionObserverInit = {
+      rootMargin: "0px",
       threshold: 1.0,
       root: null,
     };
@@ -38,3 +39,45 @@ export function Observer({ children, onContentEndVisible }: Props) {
     </div>
   );
 }
+
+
+// import React, { useEffect, useRef } from 'react';
+
+// // Опишіть Props
+// export function Observer({ children, onContentEndVisible }: Props) {
+//   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
+//   const endContentRef = useRef(null);
+
+//   useEffect(() => {
+//     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
+//     const options = {
+//       rootMargin: '0px',
+//       threshold: 1.0,
+//       root: null,
+//     };
+
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.intersectionRatio > 0) {
+//           onContentEndVisible();
+//           observer.disconnect();
+//         }
+//       });
+//     }, options);
+
+//     if (endContentRef.current) {
+//       observer.observe(endContentRef.current);
+//     }
+
+//     return () => {
+//       observer.disconnect();
+//     };
+//   }, [onContentEndVisible]);
+
+//   return (
+//     <div>
+//       {children}
+//       <div ref={endContentRef} />
+//     </div>
+//   );
+// }
